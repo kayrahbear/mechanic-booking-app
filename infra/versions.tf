c14887b5e393568ca7464ddb71cbc032c87e9cf3
Kayla <kayla.brewer@fortyau.com>
2025-04-18 19:51:45 -0500

    one thing here

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.4.2, < 1.8.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.20"   # current LTS as of Apr 2025
+      version = "~> 5.20"   # current LTS as of Apr 2025
     }
   }
 }
