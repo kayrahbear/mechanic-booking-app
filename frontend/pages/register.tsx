7791a457104b5b104cff658f94ae81be5a00b040
Kayla <kayla.brewer@fortyau.com>
2025-04-26 10:18:20 -0500

    dm fix

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -49,7 +49,7 @@ export default function Register() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-neutral-900 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-neutral-900 dark:text-white">
